f1f934e5a0fef28844586ab22c66ab90b9bdd6a3
emir.puska <>
2020-03-04 17:57:34 +0100

    Java GUI programming: initial commit

---
+++ src/one/OneExecutor.java
@@ -0,0 +1,15 @@
+package one;
+
+/**
+ * This class has no other purpose but to execute some code because this class
+ * contains main method.
+ * <p>
+ *
+ * @author grupa 3
+ */
+public class OneExecutor {
+
+    public static void main(String[] args) {
+
+    }
+}
