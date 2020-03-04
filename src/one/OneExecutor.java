85b68b19d7782ff009339b738dc1f9f2182b95bd
emir.puska <>
2020-03-04 18:01:10 +0100

    Java GUI programming: second commit

--- src/one/OneExecutor.java
+++ src/one/OneExecutor.java
@@ -4,7 +4,8 @@ package one;
  * This class has no other purpose but to execute some code because this class
  * contains main method.
  * <p>
- *
+ * Ovo je drugi komentar koji opisuje drugi commit.
+ * <p>
  * @author grupa 3
  */
 public class OneExecutor {
